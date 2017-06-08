e04cd081644da70eebddc2e556dd09c9b6ff2f45
Hon Weng Chong <hon@clinicloud.com>
2017-06-08 10:51:14 -0700

    Initial Commit

---
+++ MNIST_DRAW/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MNIST_DRAW
+//
+//  Created by Hon Weng Chong on 8/6/17.
+//  Copyright © 2017 CliniCloud Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
